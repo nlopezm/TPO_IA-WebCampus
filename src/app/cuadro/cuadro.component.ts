b77cfdd5e416ca53b475bb1dcbfe3a9adbeebf4b
nlopezm <nlopezmarini@gmail.com>
2018-10-15 12:29:20 -0300

    Initial commit

--- src/app/cuadro/cuadro.component.ts
+++ src/app/cuadro/cuadro.component.ts
@@ -6,7 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cuadro.component.scss']
 })
 export class CuadroComponent implements OnInit {
-  curso = 'WC2292996';
+  curso = '292996';
+  dia = 'VI';
+  horario = '18:30hs - 22:30hs';
+  final = '10/12/2018';
+  materia = 'PROGRAMACION ORIENTADA A OBJETOS - PROGRAMACION ORIENTADA A OBJETOS - APLICACIONES INTERACTIVAS';
+  aula = '417';
   cantAlumnos = 34;
   constructor() { }
 
